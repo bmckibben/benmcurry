bb5d97f77a005603ea30f97cbbaae1a283d105c8
bmckibben <github@literallight.com>
2023-01-28 10:25:35 -0500

    Save pre merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_01_26_131905) do
-  create_table "draws", charset: "utf8mb4", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "draws", force: :cascade do |t|
     t.datetime "draw_date"
     t.integer "b1"
     t.integer "s1"
@@ -29,7 +46,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_131905) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "picks", charset: "utf8mb4", force: :cascade do |t|
+  create_table "html_pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.boolean "active"
+    t.string "slug"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "picks", force: :cascade do |t|
     t.datetime "draw_date"
     t.integer "b1"
     t.integer "s1"
@@ -49,68 +75,109 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_131905) do
     t.integer "powerball"
     t.integer "powerscore"
     t.boolean "powermatch"
-    t.decimal "won", precision: 10
+    t.decimal "won"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "scores", charset: "utf8mb4", force: :cascade do |t|
+  create_table "quotes", force: :cascade do |t|
+    t.string "author"
+    t.text "quote"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
     t.integer "ball"
     t.integer "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", id: :integer, charset: "latin1", force: :cascade do |t|
+  create_table "section_footnotes", force: :cascade do |t|
+    t.integer "a_number"
+    t.text "footnote"
+    t.bigint "section_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["section_id"], name: "index_section_footnotes_on_section_id"
+  end
+
+  create_table "section_notes", force: :cascade do |t|
+    t.text "note"
+    t.bigint "section_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string "title"
+    t.integer "sequence"
+    t.text "summary"
+    t.text "body"
+    t.string "throughline"
+    t.text "resolution"
+    t.text "conflict"
+    t.bigint "story_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.integer "draft", default: 1
+    t.index ["story_id"], name: "index_sections_on_story_id"
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.text "title"
+    t.text "subtitle"
+    t.string "abbreviated_title"
+    t.text "short_summary"
+    t.text "long_summary"
+    t.integer "word_count"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.text "cover_image"
+    t.text "author"
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: ""
+    t.string "login", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.integer "role_cd", default: 0, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: nil
     t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at", precision: nil
     t.datetime "last_sign_in_at", precision: nil
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", precision: nil
-    t.datetime "updated_at", precision: nil
-    t.integer "congregation_id"
-    t.integer "denomination_id"
-    t.string "invitation_token"
-    t.datetime "invitation_created_at", precision: nil
-    t.datetime "invitation_sent_at", precision: nil
-    t.datetime "invitation_accepted_at", precision: nil
-    t.integer "invitation_limit"
-    t.integer "invited_by_id"
-    t.string "invited_by_type"
-    t.string "congregation_role"
-    t.integer "congregation_membership"
-    t.integer "congregation_attendance"
-    t.string "avatar"
-    t.string "first_name"
-    t.string "last_name"
-    t.boolean "tutorial_passed", default: false
-    t.string "congregation"
-    t.string "denomination"
-    t.string "location"
-    t.boolean "send_newsletter", default: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["login"], name: "index_users_on_login", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "wikis", charset: "utf8mb4", force: :cascade do |t|
+  create_table "wiki_tags", force: :cascade do |t|
+    t.integer "wiki_id"
+    t.integer "tag_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["tag_id"], name: "index_wiki_tags_on_tag_id"
+    t.index ["wiki_id"], name: "index_wiki_tags_on_wiki_id"
+  end
+
+  create_table "wikis", id: :serial, force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
     t.text "body"
     t.integer "parent"
-    t.string "version"
-    t.string "integer"
-    t.boolean "deleted"
-    t.integer "default_sort"
-    t.datetime "last_revision"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "version"
+    t.boolean "deleted", default: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.integer "default_sort", default: 0, null: false
+    t.integer "last_revision", default: 0, null: false
   end
 
 end
