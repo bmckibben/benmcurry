08f2f53f0b4f2171eb91bcc937691700a44fef0a
bmckibben <github@literallight.com>
2023-04-05 16:09:50 -0400

    remove tinymce reference

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module Bmc
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.tinymce.install = :compile
+    
   end
 end
