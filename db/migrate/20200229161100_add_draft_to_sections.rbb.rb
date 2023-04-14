12818840fe070abd0953857b289850605105660f
Ben McKibben <mckibben@att.net>
2023-04-14 13:37:31 -0400

    remove migrations

--- db/migrate/20200229161100_add_draft_to_sections.rbb.rb
+++
@@ -1,5 +0,0 @@
-class AddDraftToSections < ActiveRecord::Migration[5.2]
-  def change
-    add_column :sections, :draft, :integer, default: 1 
-  end
-end
--- db/migrate/20230125144451_create_scores.rb
+++
@@ -1,9 +0,0 @@
-class CreateScores < ActiveRecord::Migration[7.0]
-  def change
-    create_table :scores do |t|
-      t.integer :ball
-      t.integer :weight
-      t.timestamps
-    end
-  end
-end
