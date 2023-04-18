b2a3d46d9ed347598b9888d7906540a924965c14
Ben McKibben <mckibben@att.net>
2023-04-18 14:36:56 -0400

    WIP: pushing to remote repo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "jquery"
 import "@hotwired/turbo-rails"
 import "controllers"
 import "popper"
 import "bootstrap"
+
+alert($);
