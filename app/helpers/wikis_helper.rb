d2ee3b6e21a3fdd4f47c430202bb0c7186c6b279
bmckibben <github@literallight.com>
2023-07-15 11:02:17 -0400

    Turbo Rails tutorial

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,7 +1,7 @@
 module WikisHelper
 
   def nested_set(nested_set_query,link_class, parent_id)
-
+    raise nested_set_query.inspect
     current_level = 1
     first_item = true
     menu = "<ul class='wiki-toc'>"
