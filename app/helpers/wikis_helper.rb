5bd88f47302c2d3d72e3438bb896ea71ce0d6f86
bmckibben <github@literallight.com>
2023-07-23 14:29:44 -0400

    Dont show caret if wiki has not children

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,9 +1,7 @@
 module WikisHelper
   def nested_set(nested_set_query,link_class, parent_id)
 
-    if nested_set_query.empty?
-      return "&nbsp;"
-    end  
+    
     current_level = 1
     first_item = true
     if link_class == "tree-menu"  
@@ -15,25 +13,27 @@ module WikisHelper
 
     nested_set_query.each do |link|
       if !link.id.nil? and !link.list_level.nil?
-      if current_level < link.list_level
-        menu += " <ul class='wiki-toc tree'>"
-      elsif current_level > link.list_level
-        menu += "</li></ul>" * (current_level-link.list_level)
-      elsif !first_item  
-        menu += "</li>"
-      else  
-        first_item = false
-      end
-      menu += "<li>#{tree_toggler}<a href='/wikis/#{link.id}' data-wiki-id='#{link.id}' data-parent-id='#{parent_id}' class='#{link_class}'>#{link.title}</a>"
-      current_level = link.list_level
+        if current_level < link.list_level
+          menu += " <ul class='wiki-toc tree'>"
+        elsif current_level > link.list_level
+          menu += "</li></ul>" * (current_level-link.list_level)
+        elsif !first_item  
+          menu += "</li>"
+        else  
+          first_item = false
+        end
+        menu += "<li>#{tree_toggler}<a href='/wikis/#{link.id}' data-wiki-id='#{link.id}' data-parent-id='#{parent_id}' class='#{link_class}'>#{link.title}</a>"
+        current_level = link.list_level
       end
     end  
 
     menu += "</li>"
     menu += ("</ul>" * current_level)
-
-    return menu
-
+    if nested_set_query.size == 0
+      menu = "&nbsp;"
+    end  
+    
+    return  menu
   end
 
 end    
