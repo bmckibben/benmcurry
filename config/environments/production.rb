6334880ddba1fcacd5195ec9f374927f17aacbd6
Ben McKibben <github@literallight.com>
2023-09-18 14:30:45 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,24 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'www.literallight.com' }  
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+    address: "smtpout.secureserver.net",
+    port: 80,
+    domain: "www.literallight.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GD_USERNAME"],
+    password: ENV["GD_PASSWORD"]
+  }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
