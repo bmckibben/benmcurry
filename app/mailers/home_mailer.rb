a3c3b3e6a9e67d3ed9097c2034a357c0c24770c6
Ben McKibben <github@literallight.com>
2023-09-18 10:25:10 -0400

    Update home_mailer.rb

--- app/mailers/home_mailer.rb
+++ app/mailers/home_mailer.rb
@@ -3,7 +3,6 @@ class HomeMailer < ActionMailer::Base
     @email = email
     @message = message
     @name = name
-    from: @email
     mail subject: 'email for Ben Curry', to: "contact@BenMCurry.com", reply_to: @email
   end
 end
