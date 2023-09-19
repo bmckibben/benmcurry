53ccad1c0947a0ce309fdcf6c5b95b178cc7bb93
Ben McKibben <github@literallight.com>
2023-09-19 08:14:06 -0400

    Update SMTP Settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,19 +91,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'www.literallight.com' }  
+  config.action_mailer.default_url_options = { :host => 'www.benmcurry.com' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
-  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.default :charset => "utf-8"  
   config.action_mailer.smtp_settings = {  
-    address: "smtpout.secureserver.net",
-    port: 80,
-    domain: "www.literallight.com",
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "www.benmcurry.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GD_USERNAME"],
-    password: ENV["GD_PASSWORD"]
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
