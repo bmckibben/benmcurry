af7810e9d68d75e5c9a5e9e3276e6f70fe75bc91
Ben McKibben <github@literallight.com>
2023-09-19 08:15:12 -0400

    Update SMTP Settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,8 +102,8 @@ Rails.application.configure do
     domain: "www.benmcurry.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["GMAIL_SMTP_USERNAME"],
+    password: ENV["GMAIL_SMTP_PASSWORD"]
   }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
