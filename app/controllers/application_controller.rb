7bb29d8dfc6e6b248f26c824ef057af021dcbace
Ben McKibben <github@literallight.com>
2023-12-01 13:02:09 -0500

    Update application_controller.rb
    Add not_found method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
 	#before_action :authenticate_user!
 	@decendents = ""
+	def not_found
+          raise ActionController::RoutingError.new('Not Found')
+        end 	
 	private
 	def testor
 		puts "~~~~~~WTF~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
