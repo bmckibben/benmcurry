a6713e5731ec9d764539604904e9ec0b71fdb2b1
bmckibben <github@literallight.com>
2024-08-14 17:35:04 -0400

    fix permitted params error in activeadmin.stories

--- app/admin/stories.rb
+++ app/admin/stories.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register Story do
 
+	permit_params :title, :subtitle, :abbreviated_title, :shorte_summary, :long_summary, :word_count
 
 end
+
+#  id                :integer          not null, primary key
+#  title             :text
+#  subtitle          :text
+#  abbreviated_title :string(255)
+#  short_summary     :text
+#  long_summary      :text
+#  word_count        :integer
+#  created_at        :datetime
+#  updated_at        :datetime
+#
